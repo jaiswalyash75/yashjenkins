b00fe70e186ddb7f8ab86ff6bad50c87e0af7bb8
root <root@ip-172-31-44-12.ec2.internal>
2020-09-07 13:59:51 +0000

    second commit

--- linting.py
+++ linting.py
@@ -1,5 +1,5 @@
 from pylint.lint import Run
 
-results = Run(['app.py'], do_exit=False)
+results = Run(['app.py','test.py'], do_exit=False)
 # `exit` is deprecated, use `do_exit` instead
 print(results.linter.stats['global_note'])
